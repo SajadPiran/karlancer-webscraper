2fb206015f19622b3bc9e358301d78d9f7d88353
SajadPiran <sajadpiran7@gmail.com>
2025-02-06 19:54:38 -0800

    finish-notification

--- lib/Parser.py
+++ lib/Parser.py
@@ -8,7 +8,8 @@ class Parser :
         self.__soup = BeautifulSoup( self.content , 'html.parser' )
 
     def __get_all_projects( self ) -> ResultSet :
-        return self.__soup.find_all('app-sidebar-project-card')
+        class_name = 'bg-white br-9 text-right position-relative border-1-transparent card-hover p-30-20'
+        return self.__soup.find_all( 'div' , attrs= { 'class' : class_name } )
 
     def get_data_from_project( self , content : str , attr : {} = {}  ) -> list[ ResultSet ] :
 
@@ -27,13 +28,15 @@ class Parser :
         link = self.get_data_from_project( 'a' )
         title = self.get_data_from_project( 'h4' )
         release_time = self.get_data_from_project( 'span' , { 'class' : 'color-white' } )
+        price = self.get_data_from_project('div' , { 'class' : 'fs-16 b-900 mr-2 ml-1' } )
 
         for i in range( len( link ) ) :
 
             data.append( {
                 'title' : title[i].text,
                 'link' : f'https://www.karlancer.com{ link[i]['href'] } ' ,
-                'release_time' : release_time[i].text
+                'release_time' : release_time[i].text,
+                'price' : price[i].text
             } )
 
         return data
--- main.py
+++ main.py
@@ -1,14 +1,41 @@
+import re
 import threading
 import requests
+from win10toast import ToastNotifier
+
 from lib.Parser import Parser
 from lib.Request import Request
 
 url : str = 'https://www.karlancer.com/search?skillIds=1282,2915,1239,1369,2148'
+request_object : Request = Request( url, requests )
 
-if __name__ == '__main__':
+def fetch_data() -> { str : str } :
 
-    request_object = Request( url , requests )
     request = request_object.fetch_data()
     parser = Parser( request.text )
+    return parser.get_all_data()[0]
 
+def show_notification( data : dict ) :
 
+    toast = ToastNotifier()
+    toast.show_toast(
+        data['title'],
+        data['price'] ,
+        duration = 10 ,
+        icon_path = './karlancer-logo.ico'
+    )
+def start() -> { str : str } :
+
+    thread = threading.Timer( 40 , start )
+    thread.start()
+    data = fetch_data()
+    release_time =  data['release_time']
+
+    if re.search( 'ثانیه' , release_time ) is not None :
+        show_notification( data )
+        for key in data :
+            print( f'{key} + \n' )
+            
+if __name__ == '__main__':
+
+    start()
